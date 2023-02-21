a73df574bea9b0c38fcc8ab659b4e5fbe1beaaba
yishayweb <yishayweb@gmail.com>
2023-02-21 16:38:40 +0200

    using ncc and tsc

---
+++ src/func.ts
@@ -0,0 +1,3 @@
+export const niceFunction = (name: string) => {
+  console.log("hi there " + name);
+};
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { niceFunction } from "./func";
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput("who-to-greet");
+  console.log(`Hello ${nameToGreet}!`);
+  niceFunction("it");
+  const time = new Date().toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed((error as { message: string }).message);
+}
